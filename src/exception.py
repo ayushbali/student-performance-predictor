dcd9666b2843515c781600123068de1f92913035
Ayush Bali <rishu.baliab@gmail.com>
2026-09-18 19:01:31 +0200

    fixed: line 17, line 18, line 19

--- src/exception.py
+++ src/exception.py
@@ -14,9 +14,9 @@ def error_message(error, error_detail:sys):
     return error_message
 
 class CustomException(Exception):
-    def __init__(self, error_message, error_detail:sys):
-        super().__init__(error_message)
-        self.error_message = error_message(error_message, error_detail=error_detail)
+    def __init__(self, error, error_detail:sys):
+        super().__init__(error)
+        self.error_message = error_message(error, error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
